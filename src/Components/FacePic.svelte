5c405823c461ec5762bb863f6c73e8443b195184
davilivob <davilivob@gmail.com>
2023-04-03 12:31:58 +0800

    forgot to change file path in FacePic.svelte & gitignore

--- src/Components/FacePic.svelte
+++ src/Components/FacePic.svelte
@@ -4,12 +4,18 @@
     export let rounded = 'full';
     export let w = 'w-[30vh]';
     export let h = 'h-[30vh]';
-    $: img_link = `images/exhibition/head_photos/jpg/${id}.jpg`
+    $: img_link = `images/exhibition/head_photos/png/${id}.png`
 
     const go_to_person = () => {
         window.location.href = `/#/${lang}/members/${id}`;
     }
 </script>
 
-<div class="{w} {h} bg-no-repeat bg-cover bg-center rounded-{rounded} cursor-pointer" on:click={go_to_person}
-     id="image-container" style="background-image: url({img_link})"></div>
+<!--<div class="{w} {h} bg-no-repeat bg-cover bg-center rounded-{rounded} cursor-pointer"-->
+<!--     on:click={go_to_person} id="image-container"-->
+<!--     style="background-image: url({img_link})">-->
+<!--</div>-->
+
+<img src="{img_link}" alt="" class="{w} {h} rounded-{rounded} cursor-pointer"
+     on:click={go_to_person} id="image-container"
+>
