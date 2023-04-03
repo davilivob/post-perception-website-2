47c6e0516e76940beae0bcfa14d27a7a8f4913ea
davilivob <davilivob@gmail.com>
2023-04-03 23:22:05 +0800

    fix work break problem in About Page

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <div class="pt-16 flex flex-col gap-5 items-center justify-center">
-    <div class="mx-3 break-all">
+    <div class="mx-3 ">
         <h1 class="text-center text-5xl font-black my-1 text-white">
             {is_en ? "About This Exhibition" : "關於本展覽"}
         </h1>
