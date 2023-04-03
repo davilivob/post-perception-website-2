9bbc93441c2e77b35b3723c838839fdc773f3bed
davilivob <davilivob@gmail.com>
2023-04-03 17:00:38 +0800

    Get member's name in Member's page

--- src/routes/Member.svelte
+++ src/routes/Member.svelte
@@ -4,10 +4,18 @@
 
     export let params = {};
     const is_en = params.language == 'en';
-    const all_info = $information[params.language];
+    let all_info = $information[params.language];
+
+    const school_ids = all_info.school_ids;
+    let name = '';
+    Object.keys(school_ids).forEach((key) => {
+        if (school_ids[key] == params.id) name = key;
+    });
+    name = name
 
 </script>
-<div class="flex flex-row mx-10 mt-3">
-    <FacePic id={params.id} rounded="xl" w="w-[40vh]" h="h-[40vh]" lang="{params.language}"></FacePic>
-
+<div class="flex flex-col mx-10 my-12 justify-center items-center text-center gap-3">
+    <a href="/#/{params.language}/members">Go Back</a>
+    <FacePic id={params.id} rounded="xl" w="w-[40vh]" h="max-h-[40vh]" lang="{params.language}"></FacePic>
+    <i class="font-bold text-3xl">{name}</i>
 </div>
